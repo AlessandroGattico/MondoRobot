da90092d4039db457b168a1584fc9ac01dc72915
Alessandro Gattico <gattico.alessandro@gmail.com>
2022-02-11 16:01:30 +0100

    aggiunt repair

--- RobotWorld/src/model/Robot.java
+++ RobotWorld/src/model/Robot.java
@@ -84,35 +84,103 @@ public class Robot extends Character {
         switch (this.facing) {
             case NORTH:
                 if (this.cell.getNeighbours()[0] != null) {
-                    this.changeRobotCell(this.cell.getNeighbours()[0]);
+                    if (this.cell.getNeighbours()[0].getItem() != Items.EMPTY) {
+                        this.changeRobotCell(this.cell.getNeighbours()[0]);
+                    } else {
+                        this.support.firePropertyChange("bump", 0, 0);
+                    }
                 }
                 break;
             case EAST:
                 if (this.cell.getNeighbours()[1] != null) {
-                    this.changeRobotCell(this.cell.getNeighbours()[1]);
+                    if (this.cell.getNeighbours()[1].getItem() != Items.EMPTY) {
+                        this.changeRobotCell(this.cell.getNeighbours()[1]);
+                    } else {
+                        this.support.firePropertyChange("bump", 0, 0);
+                    }
                 }
                 break;
             case SOUTH:
                 if (this.cell.getNeighbours()[2] != null) {
-                    this.changeRobotCell(this.cell.getNeighbours()[2]);
+                    if (this.cell.getNeighbours()[2].getItem() != Items.EMPTY) {
+                        this.changeRobotCell(this.cell.getNeighbours()[2]);
+                    } else {
+                        this.support.firePropertyChange("bump", 0, 0);
+                    }
                 }
                 break;
             case WEST:
                 if (this.cell.getNeighbours()[3] != null) {
-                    this.changeRobotCell(this.cell.getNeighbours()[3]);
+                    if (this.cell.getNeighbours()[3].getItem() != Items.EMPTY) {
+                        this.changeRobotCell(this.cell.getNeighbours()[3]);
+                    } else {
+                        this.support.firePropertyChange("bump", 0, 0);
+                    }
                 }
                 break;
             default:
                 break;
         }
-
         this.support.firePropertyChange("move", oldCell, this.cell);
-
     }
 
     private void changeRobotCell(Cell neighbour) {
         this.cell.deleteItem();
         this.cell = neighbour;
         this.cell.addItem(Items.ROBOT);
+    }
+
+    public void turOn() {
+        switch (this.facing) {
+            case NORTH:
+                if (this.cell.getNeighbours()[0] != null) {
+                    this.support.firePropertyChange("on", this.cell, this.cell.getNeighbours()[0]);
+                }
+                break;
+            case EAST:
+                if (this.cell.getNeighbours()[1] != null) {
+                    this.support.firePropertyChange("on", this.cell, this.cell.getNeighbours()[1]);
+                }
+                break;
+            case SOUTH:
+                if (this.cell.getNeighbours()[2] != null) {
+                    this.support.firePropertyChange("on", this.cell, this.cell.getNeighbours()[2]);
+                }
+                break;
+            case WEST:
+                if (this.cell.getNeighbours()[3] != null) {
+                    this.support.firePropertyChange("on", this.cell, this.cell.getNeighbours()[3]);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void repair() {
+        switch (this.facing) {
+            case NORTH:
+                if (this.cell.getNeighbours()[0] != null) {
+                    this.support.firePropertyChange("repair", this.cell, this.cell.getNeighbours()[0]);
+                }
+                break;
+            case EAST:
+                if (this.cell.getNeighbours()[1] != null) {
+                    this.support.firePropertyChange("repair", this.cell, this.cell.getNeighbours()[1]);
+                }
+                break;
+            case SOUTH:
+                if (this.cell.getNeighbours()[2] != null) {
+                    this.support.firePropertyChange("repair", this.cell, this.cell.getNeighbours()[2]);
+                }
+                break;
+            case WEST:
+                if (this.cell.getNeighbours()[3] != null) {
+                    this.support.firePropertyChange("shoot", this.cell, this.cell.getNeighbours()[3]);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
